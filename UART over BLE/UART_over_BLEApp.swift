4490fb95d8935e78b4a083bb841a4c26374877cb
berra armutlu <armutluberra@gmail.com>
2026-07-13 15:00:55 +0300

    Initial Commit

---
+++ UART over BLE/UART_over_BLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  UART_over_BLEApp.swift
+//  UART over BLE
+//
+//  Created by Berra Armutlu on 13.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct UART_over_BLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
